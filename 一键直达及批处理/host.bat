16033263ca5b353744b4c0162b5c6bc55b6fb83d
master <51758018@qq.com>
2018-08-10 10:13:40 +0800

    add

---
+++ 一键直达及批处理/host.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Notepad++\notepad++.exe" "C:\Windows\System32\drivers\etc\hosts"
---
+++ 一键直达及批处理/openAllSoft.bat
@@ -0,0 +1,8 @@
+@echo off
+
+start "" "C:\Program Files (x86)\Tencent\QQ\Bin\QQ.exe"
+start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
+start "" "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe"
+start "" "C:\ДњТы\SERP\SERP3.0\SERP3.0.sln"
+
+exit
