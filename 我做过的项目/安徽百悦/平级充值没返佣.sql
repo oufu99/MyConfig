2f6084f167b6ad5d9ddb1a7d7b5a7d28bdecf48b
master <51758018@qq.com>
2018-08-13 17:54:00 +0800

    a

---
+++ 我做过的项目/安徽百悦/平级充值没返佣.sql
@@ -0,0 +1,12 @@
+SELECT oldparent_id, upLevel_time,* FROM tb_customer_10563 WHERE  tb_customerID=167
+SELECT oldparent_id, upLevel_time,* FROM tb_customer_10563 WHERE tb_customerID=108
+
+
+
+
+ SELECT * FROM dbo.tb_recommend_money_history_10563 WHERE ordered_custId=167
+    SELECT * FROM dbo.cust_personal_center_in_10596
+
+	SELECT * FROM cust_payment_order_10563 WHERE cust_id=167
+
+	SELECT * FROM cust_payment_order_10563 WHERE orderNum='201808060753076252'
