8144d3b8b00a0f4b3e5334f463f179f484f2c2ac
master <51758018@qq.com>
2018-08-21 17:54:44 +0800

    aa

---
+++ 我做过的项目/优旭生物/优旭生物.sql
@@ -0,0 +1,8 @@
+--UPDATE  tb_rule_mapping_class SET manu_id=10602 WHERE manu_id=-10602 and  ID IN(4871,4872)
+
+SELECT * FROM tb_customer_10602
+
+
+SELECT * FROM tb_user WHERE manufacturer_id=10602
+
+SELECT * FROM dbo.tb_customer_catalog where  manufacturer_id=10602
