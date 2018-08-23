6088d8e924df5417dcba9aa60442985d6bc8804c
oufu99 <51758018@qq.com>
2018-08-23 16:32:50 +0800

    add

---
+++ 查询问题的SQL如查红包被谁扫/红包被谁扫.sql
@@ -0,0 +1,7 @@
+EXEC SActivityInfo '4098885347878757' 
+
+
+
+SELECT * FROM tb_activity_history_1026_10065  where identity_code='4098885347878757'
+SELECT * FROM tb_activity_prize_history_1026_10065 where identity_code='4098885347878757'
+SELECT * FROM dbo.tb_activity_redPacket_send_result_1026_10065 where identity_code='4098885347878757' --红包返回结果表
