cb6d5750a8f49a8a06ba922c2ae518c9e5cf2b38
oufu99 <51758018@qq.com>
2018-09-27 19:00:35 +0800

    aa

---
+++ 我做过的项目/舒仑朵/清除库存数据.sql
@@ -0,0 +1,8 @@
+ 
+
+
+--UPDATE  tb_stock_10532 SET virtual_stock=0
+--UPDATE  dbo.tb_manualModifyStock_10532 SET 
+--SELECT * FROM tb_stock_in_10532
+--delete stock_in_or_out_10532
+ SELECT * FROM tb_stock_in_10532
