1b1939b610162b82f8b5366b43cd51677dcfedfb
oufu99 <51758018@qq.com>
2018-09-28 16:22:19 +0800

    a

--- 我做过的项目/舒仑朵/清除库存数据.sql
+++ 我做过的项目/舒仑朵/清除库存数据.sql
@@ -1,8 +1,17 @@
  
 
 
+ --Çå³ý¶©µ¥
+--SELECT * FROM tb_order_10532 WHERE Order_num='36505820180926'	 
+--UPDATE  tb_order_10532 SET is_active=0 WHERE Order_num='36505820180926'
+
 --UPDATE  tb_stock_10532 SET virtual_stock=0
 --UPDATE  dbo.tb_manualModifyStock_10532 SET 
 --SELECT * FROM tb_stock_in_10532
 --delete stock_in_or_out_10532
+
  SELECT * FROM tb_stock_in_10532
+
+
+ 
+ 
