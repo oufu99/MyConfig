16f8409e33dc69ca25c46e59505356729be4cc6f
oufu99 <51758018@qq.com>
2018-10-30 16:33:18 +0800

    add

--- 一键直达及批处理/openAllSoft.bat
+++ 一键直达及批处理/openAllSoft.bat
@@ -2,7 +2,8 @@
 
 start "" "C:\Program Files (x86)\Tencent\QQ\Bin\QQ.exe"
 start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
-start "" "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe"
-start "" "C:\ДњТы\SERP\SERP3.0\SERP3.0.sln"
+start "" "C:\Program Files\PremiumSoft\Navicat Premium 12\navicat.exe"
+:: "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe" 
+start "" "E:\ZPCode\SuYa_V2\SuYa_V2.sln"
 
 exit
