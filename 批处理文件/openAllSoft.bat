e1744b1550060eb84412add7d5a2a2666cc7aadb
oufu99 <51758018@qq.com>
2018-11-12 17:52:32 +0800

    添加replace支持

--- 批处理文件/openAllSoft.bat
+++ 批处理文件/openAllSoft.bat
@@ -5,6 +5,6 @@ start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
 start "" "C:\Program Files\PremiumSoft\Navicat Premium 12\navicat.exe"
 :: "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe" 
 start "" "E:\ZPCode\SuYa_V2\SuYa_V2.sln"
-start "" "D:\MyConfig\我的配置文件\InitProgram.ahk"
+start "" "D:\MyConfig\批处理文件\InitProgram.ahk"
 
 exit
