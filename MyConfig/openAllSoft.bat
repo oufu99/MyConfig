aeaddc6fe535de4b6b7b9f26cfaf691c082f5b27
oufu99 <51758018@qq.com>
2018-12-24 17:54:22 +0800

    更新vsvim配置

--- MyConfig/openAllSoft.bat
+++ MyConfig/openAllSoft.bat
@@ -3,15 +3,16 @@
 start "" "D:\MyLove\VimD\userPlugins\InitProgram.ahk"
 start "" "C:\Program Files (x86)\Tencent\QQ\Bin\QQ.exe"
 start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
-start "" "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe"
+
 start "" "D:\MyLove\VimD\vimd.exe"
 start "" "E:\ZPCode\SuYa_V2\SuYa_V2.sln"
 start "" "C:\Program Files\Listary\Listary.exe"
 :: vid有点问题先关闭再打开
-:: taskkill /f /im vimd.exe
-:: start "" "D:\MyLove\VimD\vimd.exe"
-
+taskkill /f /im vimd.exe
+start "" "D:\MyLove\VimD\vimd.exe"
+start "" "C:\Program Files\PremiumSoft\Navicat Premium 12\navicat.exe" 
 exit
 
-:: start "" "C:\Program Files\PremiumSoft\Navicat Premium 12\navicat.exe" 
+
+:: start "" "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe"
 :: start "" "E:\ZPCode\SERP3.0\SERP3.0.sln"
