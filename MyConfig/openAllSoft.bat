e56311a90263c072a5452eb0803c8f211701738c
oufu99 <51758018@qq.com>
2019-03-22 15:57:59 +0800

    添加buf

--- MyConfig/openAllSoft.bat
+++ MyConfig/openAllSoft.bat
@@ -15,8 +15,10 @@ start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
 start "" "D:\MyLove\VimD\vimd.exe"
 
 :: start "" "E:\ZPCode\SERP3.0\SERP3.0.sln"
-start "" "E:\ZPCode\SuYa_V2\SuYa_V2.sln"
+:: start "" "E:\ZPCode\SuYa_V2\SuYa_V2.sln"
 :: start "" "E:\ZPCode\Mifei_v2\Mifei_v2.sln"
+start "" "E:\ZPCode\zp.ymt\ZP.YMT.sln"
+
 
 start "" "C:\Program Files\Listary\Listary.exe"
 :: vid有点问题先关闭再打开
