03487d052fa0a34802b07a56a995dbde99463030
oufu99 <51758018@qq.com>
2019-05-07 17:11:30 +0800

    优化

--- MyConfig/openAllSoft.bat
+++ MyConfig/openAllSoft.bat
@@ -7,10 +7,9 @@ cd /d %~dp0
 
 :: 两个::是注释
 start "" "D:\MyLove\VimD\userPlugins\InitProgram.ahk"
-:: 两个qq号
-start "" "C:\Program Files (x86)\Tencent\QQ\Bin\QQ.exe"
-start "" "C:\Program Files (x86)\Tencent\QQ\Bin\QQ.exe"
 start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
+start "" "C:\Program Files\Listary\Listary.exe"
+
 
 start "" "D:\MyLove\VimD\vimd.exe"
 
@@ -27,4 +26,9 @@ start "" "D:\MyLove\VimD\vimd.exe"
 start "" "C:\Program Files\PremiumSoft\Navicat Premium 12\navicat.exe" 
 :: start "" "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe"
 start "" "D:\MyLove\VimD\userPlugins\CodePrompt\CodePrompt.exe"
+
+
+
+:: 打开qq
+start "" "D:\MyLove\VimD\userPlugins\OpenQQ.ahk"
 exit
