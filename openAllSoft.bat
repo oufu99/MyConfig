4d3fd2df5cb69bc90da145d6e65b96ace3f0715b
oufu99 <51758018@qq.com>
2019-06-12 14:50:47 +0800

    添加代码

---
+++ openAllSoft.bat
@@ -0,0 +1,34 @@
+@echo off
+:: 获取管理员权限
+cd /d %~dp0
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+
+
+
+:: 两个::是注释
+start "" "D:\MyLove\VimD\userPlugins\InitProgram.ahk"
+start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
+start "" "C:\Program Files\Listary\Listary.exe"
+
+
+start "" "D:\MyLove\VimD\vimd.exe"
+
+:: start "" "E:\ZPCode\SERP3.0\SERP3.0.sln"
+:: start "" "E:\ZPCode\SuYa_V2\SuYa_V2.sln"
+:: start "" "E:\ZPCode\Mifei_v2\Mifei_v2.sln"
+start "" "E:\ZPCode\zp.ymt\ZP.YMT.sln"
+
+
+start "" "C:\Program Files\Listary\Listary.exe"
+:: vid有点问题先关闭再打开
+taskkill /f /im vimd.exe
+start "" "D:\MyLove\VimD\vimd.exe"
+start "" "C:\Program Files\PremiumSoft\Navicat Premium 12\navicat.exe" 
+:: start "" "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe"
+:: start "" "D:\MyLove\VimD\userPlugins\CodePrompt\CodePrompt.exe"
+
+
+
+:: 打开qq
+start "" "D:\MyLove\VimD\userPlugins\OpenQQ.ahk"
+exit
