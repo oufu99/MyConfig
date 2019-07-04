f688cac50fa5547c0f032123a338709d80804c64
oufu99 <51758018@qq.com>
2019-07-04 16:59:42 +0800

    整理代码段

--- MyConfig/openAllSoft.bat
+++ MyConfig/openAllSoft.bat
@@ -13,20 +13,21 @@ start "" "C:\Program Files\Listary\Listary.exe"
 
 start "" "D:\MyLove\VimD\vimd.exe"
 
-:: start "" "E:\ZPCode\SERP3.0\SERP3.0.sln"
-:: start "" "E:\ZPCode\SuYa_V2\SuYa_V2.sln"
-:: start "" "E:\ZPCode\Mifei_v2\Mifei_v2.sln"
-start "" "E:\ZPCode\zp.ymt\ZP.YMT.sln"
-
 
 start "" "C:\Program Files\Listary\Listary.exe"
 :: vid有点问题先关闭再打开
 taskkill /f /im vimd.exe
 start "" "D:\MyLove\VimD\vimd.exe"
-start "" "C:\Program Files\Navicat Premium 12 notsetup\navicat.exe" 
-:: start "" "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe"
-:: start "" "D:\MyLove\VimD\userPlugins\CodePrompt\CodePrompt.exe"
 
+
+::  3.0项目
+start "" "E:\ZPCode\SERP3.0\SERP3.0.sln"
+start "" "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe"
+
+
+::  4.0活动
+:: start "" "E:\ZPCode\zp.ymt\ZP.YMT.sln"
+:: start "" "C:\Program Files\Navicat Premium 12 notsetup\navicat.exe" 
 
 
 :: 打开qq
