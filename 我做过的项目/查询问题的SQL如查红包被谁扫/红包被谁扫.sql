79d7bf5da3bb389772028b102d21e27e041cb3d9
打野的小妞 <oufu99@gmail.com>
2019-11-02 12:21:41 +0800

    删除正品的东西

--- 我做过的项目/查询问题的SQL如查红包被谁扫/红包被谁扫.sql
+++
@@ -1,7 +0,0 @@
-EXEC SActivityInfo '4098885347878757' 
-
-
-
-SELECT * FROM tb_activity_history_1026_10065  where identity_code='4098885347878757'
-SELECT * FROM tb_activity_prize_history_1026_10065 where identity_code='4098885347878757'
-SELECT * FROM dbo.tb_activity_redPacket_send_result_1026_10065 where identity_code='4098885347878757' --红包返回结果表
--- 我做过的项目/标准模板/添加PC结转.sql
+++
@@ -1,7 +0,0 @@
- 
-INSERT dbo.tb_manumethod_setting
-        ( manu_id, dllname, namespace )
-VALUES  ( N-'10532', -- manu_id - nvarchar(50)
-          N'SERP3.0.shulunduo.PC', -- dllname - nvarchar(50)
-          N'SERP3._0.shulunduo.PC'  -- namespace - nvarchar(50)
-          )
