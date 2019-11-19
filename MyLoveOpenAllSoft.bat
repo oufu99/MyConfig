1fef27afe54fedd120bc454a61dabcbcc30ac8ab
oujialin <oujialin@dossen.com>
2019-11-19 18:27:57 +0800

    aa

--- MyLoveOpenAllSoft.bat
+++ MyLoveOpenAllSoft.bat
@@ -6,22 +6,14 @@ cd /d %~dp0
 
 
 :: 两个::是注释
-start "" "D:\Common\VimD\userPlugins\InitProgram.ahk"
+start "" "D:\MyLove\AHK\StartAllMyLoveAhk.ahk"
 start "" "D:\Common\VimD\vimd.exe"
 start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
 start "" "C:\Program Files\Listary\Listary.exe"
-
-
-
-:: start "" "E:\ZPCode\zp.ymt\ZP.YMT.sln"
-
 :: vid有点问题先关闭再打开
 taskkill /f /im vimd.exe
 start "" "D:\Common\VimD\vimd.exe"
-start "" "D:\software\Navicat Premium 12\navicat.exe" 
-:: start "" "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe"
-:: start "" "D:\Common\VimD\userPlugins\CodePrompt\CodePrompt.exe"
-
+:: start "" "D:\software\Navicat Premium 12\navicat.exe" 
 :: 打开qq
 start "" "D:\MyLove\AHK\OpenMyLoveQQ.ahk"
 exit
