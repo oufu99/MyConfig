192d060468f0d472a514ebffa03ad41ce6dc77fd
oujialin <oujialin@dossen.com>
2019-11-22 09:07:31 +0800

    aa

--- MyLoveOpenAllSoft.bat
+++ MyLoveOpenAllSoft.bat
@@ -13,7 +13,9 @@ start "" "C:\Program Files\Listary\Listary.exe"
 :: vid有点问题先关闭再打开
 taskkill /f /im vimd.exe
 start "" "D:\Common\VimD\vimd.exe"
-:: start "" "D:\software\Navicat Premium 12\navicat.exe" 
+start "" "D:\software\Navicat Premium 12\navicat.exe" 
+start "" "D:\software\WizNote\Wiz.exe"
+start "" "F:\Dossen\OTA-Interface\Web.Interface.sln"
 :: 打开qq
-start "" "D:\MyLove\AHK\OpenMyLoveQQ.ahk"
+start "" "D:\MyLove\AHK\OpenMyLoveQQ.ahk" 
 exit
