c31976151ae73bcab8d12d75462015d224683592
打野的小妞 <oufu99@gmail.com>
2020-01-13 22:48:48 +0800

    aaron commit

--- MyLooveOpenAllSoft.bat
+++ MyLooveOpenAllSoft.bat
@@ -4,7 +4,7 @@ cd /d %~dp0
 %1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
 
 :: 两个::是注释
-start "" "D:\MyLoove\AHK\StartAllMyLooveAhk.ahk"
+start "" "D:\MyLoove\AHK\MyLoove.ahk"
 :: start "" "D:\MyLoove\AHK\OpenMyLooveQQ.ahk"
 start "" "D:\Common\VimD\vimd.exe"
 :: vid有点问题先关闭再打开
